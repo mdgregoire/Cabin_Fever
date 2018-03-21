32c9f80cd47dd1b972df538cc35ad40b6d8de8e1
Michael Gregoire <mdgregoire@gmail.com>
2018-03-21 16:07:43 -0500

    upload toggle

--- server/public/scripts/controllers/opening_list.controller.js
+++ server/public/scripts/controllers/opening_list.controller.js
@@ -8,34 +8,37 @@ myApp.controller('Opening_ListController',  ['UserService', 'ViewPropertyService
   self.displayProperty = ViewPropertyService.displayProperty;
   self.displayCabin = ViewPropertyService.displayCabin;
   let cabinId = self.displayCabin.cabin[0].id;
-  console.log(cabinId, 'cabin in ol cont');
+  self.showUpload = {show: false};
 
+  self.uploadFiles = function(file, errFiles) {
+    self.f = file;
+    self.errFile = errFiles && errFiles[0];
+    if (file) {
+      file.upload = Upload.upload({
+          url: `/upload/${cabinId}`,
+          data: {file: file}
+      });
+    file.upload.then(function (response) {
+        $timeout(function () {
+            file.result = response.data;
+        });
+    }, function (response) {
+        if (response.status > 0)
+            self.errorMsg = response.status + ': ' + response.data;
+    }, function (evt) {
+        file.progress = Math.min(100, parseInt(100.0 *
+                                 evt.loaded / evt.total));
+    });
+    }
+    alert('File Uploaded!');
+    self.showUpload.show = false;
+  }
+//end uploadFiles
 
-
-      self.uploadFiles = function(file, errFiles) {
-
-          self.f = file;
-          self.errFile = errFiles && errFiles[0];
-          if (file) {
-              file.upload = Upload.upload({
-                  url: `/upload/${cabinId}`,
-                  data: {file: file}
-              });
-
-              file.upload.then(function (response) {
-                  $timeout(function () {
-                      file.result = response.data;
-                  });
-              }, function (response) {
-                  if (response.status > 0)
-                      self.errorMsg = response.status + ': ' + response.data;
-              }, function (evt) {
-                  file.progress = Math.min(100, parseInt(100.0 *
-                                           evt.loaded / evt.total));
-              });
-          }
-      }
-
+self.uploadToggle = function(){
+  console.log('in uploadToggle', self.showUpload.show);
+  self.showUpload.show = true;
+}
 
 
 
